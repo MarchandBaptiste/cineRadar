81ce6c42e0cae0368dcace72f1a19255722d4522
Marchand <baptistemarchand2007@gmail.com>
2025-12-10 13:08:43 +0100

    filtrebygenremovie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
 // ATTENTION DE PAS PUSH CA SUR GITHUB
 const API_KEY =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTc5NTU0ZjJjY2RkYWQ4ZmE0ZjE0NmM3NTE3ZjFhYiIsIm5iZiI6MTc2NDY4NjAzOS4zMTgsInN1YiI6IjY5MmVmOGQ3OTE4NTk3ZDVkNzM5OTM2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Nqb8PJ0fxYqZPOJIy-Zw_KrXNxUhwbjV6S841_cB5Fc";
+// 
 
 let popularMovies = [];
 const selectElement = document.querySelector("#genre-select");
+const selectMovies = document.querySelector("#loadMovies");
+
+let options = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + API_KEY,
+  },
+};
 
 const getMovies = async () => {
-  let options = {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + API_KEY,
-    },
-  };
-
   const promise = await fetch(
     "https://api.themoviedb.org/3/genre/movie/list?language=fr",
     options
@@ -32,9 +34,40 @@ const loadGenres = async () => {
 
 loadGenres();
 
-// local storage
-// ne pas mettre sa clé d'api sur gitub
-// et voir api reférence cliquer sur node et try it
-// pour le select mettre un input en addeventlistener
-// charger tous les films avec un seul url et faire un double trie (genre et popularité)
-// local storage poyur enregistrer les film déja vue (il faut trasfomer en chaine de caractère le json stringify ) j'uste ca fonctione
+selectElement.addEventListener("input", async (event) => {
+  const moviesData = await byGenres(event.target.value);
+  loadMovies(moviesData.results);
+});
+
+async function byGenres(value) {
+  const listMovies = await fetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&with_genres=" +
+      value,
+    options
+  );
+  return await listMovies.json();
+}
+
+function loadMovies(movies) {
+  selectMovies.innerHTML = "";
+
+  movies.forEach((movie) => {
+    const posterUrl = movie.poster_path
+      ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+      : "https://via.placeholder.com/300x450?text=Pas+d'Image";
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("movie-card");
+
+    movieElement.innerHTML = `
+      <img src="${posterUrl}" alt="Affiche de ${movie.title}" />
+      <h3>${movie.title}</h3>
+      <p>Note : ${movie.vote_average.toFixed(1)}/10</p>
+      <p>${
+        movie.release_date
+          ? movie.release_date.substring(0, 4)
+          : "Date inconnue"
+      }</p>
+    `;
+    selectMovies.appendChild(movieElement);
+  });
+}
