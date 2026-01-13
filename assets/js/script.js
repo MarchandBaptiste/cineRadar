0e609301be99013e426b05ed073d97a7ae602b54
Marchand <baptistemarchand2007@gmail.com>
2026-01-13 16:38:49 +0100

    changement image

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,8 +113,8 @@ function createMovieCard(movie) {
     }</p>
   `;
 
-  const imageInitiale = "/assets/img/heart.png";
-  const imageSurvole = "/assets/img/heartVide.png";
+  const imageInitiale = "https://img.icons8.com/ios/50/ff0000/like--v1.png";
+  const imageSurvole = "https://img.icons8.com/ios-filled/50/ff0000/like--v1.png";
 
   const image = document.createElement("img");
   image.style.cursor = "pointer";
@@ -179,8 +179,8 @@ function createSerieCard(serie) {
     }</p>
   `;
 
-  const imageInitiale = "/assets/img/heart.png";
-  const imageSurvole = "/assets/img/heartVide.png";
+  const imageInitiale = "https://img.icons8.com/ios/50/ff0000/like--v1.png";
+  const imageSurvole = "https://img.icons8.com/ios-filled/50/ff0000/like--v1.png";
 
   const image = document.createElement("img");
   image.style.cursor = "pointer";
@@ -267,7 +267,7 @@ if (containerMovies && selectGenreMovies) {
 
   selectGenreMovies.addEventListener("input", async (e) => {
     if (e.target.value === "") {
-      const data = await getPopularMovies(); //si tous alors charge les pouliare 
+      const data = await getPopularMovies(); //si tous alors charge les pouliare
       renderMovies(data.results);
     } else {
       const data = await getMoviesByGenre(e.target.value); //sinon charge par genre
