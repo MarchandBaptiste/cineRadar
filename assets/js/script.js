c0bbf5c4e3fffe96af08eb730be836d39dc81579
Marchand <baptistemarchand2007@gmail.com>
2026-01-13 16:46:57 +0100

    test image coeur changement de chemin

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,8 +113,8 @@ function createMovieCard(movie) {
     }</p>
   `;
 
-  const imageInitiale = "https://img.icons8.com/ios/50/ff0000/like--v1.png";
-  const imageSurvole = "https://img.icons8.com/ios-filled/50/ff0000/like--v1.png";
+  const imageInitiale = "assets/img/heart.png";
+  const imageSurvole = "assets/img/heartVide.png";
 
   const image = document.createElement("img");
   image.style.cursor = "pointer";
@@ -179,8 +179,8 @@ function createSerieCard(serie) {
     }</p>
   `;
 
-  const imageInitiale = "https://img.icons8.com/ios/50/ff0000/like--v1.png";
-  const imageSurvole = "https://img.icons8.com/ios-filled/50/ff0000/like--v1.png";
+  const imageInitiale = "assets/img/heart.png";
+  const imageSurvole = "assets/img/heartVide.png";
 
   const image = document.createElement("img");
   image.style.cursor = "pointer";
